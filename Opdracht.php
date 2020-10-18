5623b90b9701c015580e457e58143bd096547b5d
pieterbro05 <61417878+pieterbro05@users.noreply.github.com>
2020-10-18 16:27:52 +0200

    Update Opdracht.php

--- Opdracht.php
+++ Opdracht.php
@@ -11,6 +11,26 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
+$this->phpserial->deviceSet("/dev/tty.usbmodem411");	
+// We can change the baud rate
+$this->phpserial->confBaudRate(115200); 	
+		
+		
+// Then we need to open it
+$this->phpserial->deviceOpen();
+		
+// To write into
+//$this->phpserial->sendMessage("r");
+		
+// Or to read from
+$read = $this->phpserial->readPort();
+		
+$line = $this->phpserial->readPort();
+			
+print_r($line);
+		
+// If you want to change the configuration, the device must be closed
+$this->phpserial->deviceClose();		
 
 $Waarde = filter_input(INPUT_POST, 'Waarde');
 $ID = filter_input(INPUT_POST, 'ID');
