6029d5dfbe4a150b3b143b4c04f74bf30b3fb5bd
pieterbro05 <61417878+pieterbro05@users.noreply.github.com>
2020-11-08 19:21:01 +0100

    Update arduinoFile.ino

--- arduinoFile.ino
+++ arduinoFile.ino
@@ -1,14 +1,15 @@
 
 const int TemperatuurPin = A0;
 const int LichtPin = A1;
-unsigned long time;
+unsigned long long time;
 const double k = 5.0/1024;
 const double luxFactor = 500000;
 const double R2 = 220;
 const double LowLightLimit = 200; 
 const double B = 1.3*pow(10.0,7);
 const double m = -1.4;
-
+unsigned long long vorigetijdT=0;
+unsigned long long vorigetijdL=0;
 double Light (int RawADC0)
 {
   double V2 = k*RawADC0;
@@ -27,15 +28,19 @@ void loop()
   time = millis();
   int temp = analogRead(TemperatuurPin);
   temp = temp * 0.48828125;
-  if(time%60000*5==0)
+  if(time-vorigetijdT>=5*60000)
   {
+    vorigetijdT=time;
     Serial.print(1);
     Serial.println(temp);
    
   }
-  if(time%60000*2==0)
+  if(time-vorigetijdL>=2*60000)
   {
-    Serial.print(0);
+    vorigetijdL=time;
+    Serial.print(2);
     Serial.println(int(Light(analogRead(LichtPin))));
   }
 }
+
+ 
