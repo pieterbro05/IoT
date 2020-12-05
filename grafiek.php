65999f47b4b3a8cef2e3cb1c574a442a77c8c471
pieterbro05 <61417878+pieterbro05@users.noreply.github.com>
2020-12-05 17:33:53 +0100

    Update grafiek.php

--- grafiek.php
+++ grafiek.php
@@ -7,15 +7,26 @@ $conn = new mysqli($servername, $username, $password, $dbname);// Check connecti
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
-$IDfilter=1;
-$sql="SELECT * FROM DataTabel WHERE ID = '".$IDfilter."'";
+
+$sql="SELECT * FROM DataTabel WHERE ID = 1";
 $result = mysqli_query($conn,$sql);
 
-
+$dataPoints=array();
 while($row = mysqli_fetch_array($result)) {
-	$dataPoints=array(array("x"=>$row['Tijd'],"y"=>$row['Waarde']));
+	$Time=strtotime($row['Tijd'])*1000;
+	$dataPoints[]=array("x"=>$Time,"y"=>$row['Waarde']);
    	
 }
+$sql="SELECT * FROM DataTabel WHERE ID = 2";
+$result = mysqli_query($conn,$sql);
+
+$dataPoints2=array();
+while($row = mysqli_fetch_array($result)) {
+	$Time=strtotime($row['Tijd'])*1000;
+	$dataPoints2[]=array("x"=>$Time,"y"=>$row['Waarde']);
+   	
+}
+
 
 mysqli_close($conn);
  
@@ -26,26 +37,70 @@ mysqli_close($conn);
 <script>
 window.onload = function () {
  
-var chart = new CanvasJS.Chart("chartContainer", {
+var options = {
 	animationEnabled: true,
+	exportEnabled: true,
+	zoomEnabled: true,
 	title:{
 		text: "jQuery Grafiek"
 	},
 	axisY: {
 		title: "Waarde",
-		valueFormatString: "#0,,.",
-		suffix: "°C"
+		valueFormatString: "#,##0.##",
+		suffix: ""
 	
+	},
+	axisX:{
+		title: "Tijd"
+	},
+	toolTip: {
+		shared: true
+	},
+	legend: {
+		cursor: "pointer",
+		itemclick: toggleDataSeries
 	},
 	data: [{
 		type: "spline",
-		markerSize: 5,
+		name: "Temperatuur",
+		markerSize: 1,
 		xValueFormatString: "YYYY",
-		yValueFormatString: "$#,##0.##",
+		yValueFormatString: "#,##0.##°C",
 		xValueType: "dateTime",
+		showInLegend: true,
 		dataPoints: <?php echo json_encode($dataPoints, JSON_NUMERIC_CHECK); ?>
+	},{
+		type: "spline",
+		name: "Licht",
+		markerSize: 1,
+		xValueFormatString: "YYYY",
+		yValueFormatString: "#,##0.##lm",
+		xValueType: "dateTime",
+		showInLegend: true,
+		dataPoints: <?php echo json_encode($dataPoints2, JSON_NUMERIC_CHECK); ?>
 	}]
-});
+};
+ 
+$("#chartContainer").CanvasJSChart(options);
+
+function toggleDataSeries(e) {
+	if (typeof (e.dataSeries.visible) === "undefined" || e.dataSeries.visible) {
+		e.dataSeries.visible = false;
+	} else {
+		e.dataSeries.visible = true;
+	}
+	e.chart.render();
+}
+ 
+}
+</script>
+</head>
+<body>
+<div id="chartContainer" style="height: 370px; width: 100%;"></div>
+<script src="https://canvasjs.com/assets/script/jquery-1.11.1.min.js"></script>
+<script src="https://canvasjs.com/assets/script/jquery.canvasjs.min.js"></script>
+</body>
+</html> 
  
 chart.render();
  
