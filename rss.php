676a72c9e5a898fa01a799faab231c6fe65382a2
pieterbro05 <61417878+pieterbro05@users.noreply.github.com>
2020-12-05 17:35:39 +0100

    Update rss.php

--- rss.php
+++ rss.php
@@ -9,9 +9,35 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-$sql= mysqli_query($conn, "SELECT MAX( Waarde ) AS max FROM `DataTabel`;" );
+$sql= mysqli_query($conn, "SELECT MAX( Waarde ) AS max FROM `DataTabel`WHERE ID =1;" );
 $res = mysqli_fetch_array( $sql);
-$maximum = $res['max'];
+$maximumtemp = $res['max'];
+$sql= mysqli_query($conn, "SELECT MIN( Waarde ) AS min FROM `DataTabel` WHERE ID =1;" );
+$res = mysqli_fetch_array( $sql);
+$minimumtemp = $res['min'];
+$sql= mysqli_query($conn, "SELECT AVG(Waarde) AS AveragePrice FROM DataTabel WHERE ID =1;" );
+$res = mysqli_fetch_array($sql);
+$gemtemp = $res['AveragePrice'];
+$sql="SELECT * FROM DataTabel WHERE ID = 1";
+$result = mysqli_query($conn,$sql);
+while($row = mysqli_fetch_array($result)) {
+	$lasttemp=$row['Waarde'];	
+}
+
+$sql= mysqli_query($conn, "SELECT MAX( Waarde ) AS max FROM `DataTabel`WHERE ID =2;" );
+$res = mysqli_fetch_array( $sql);
+$maximumlicht = $res['max'];
+$sql= mysqli_query($conn, "SELECT MIN( Waarde ) AS min FROM `DataTabel` WHERE ID =2;" );
+$res = mysqli_fetch_array($sql);
+$minimumlicht = $res['min'];
+$sql= mysqli_query($conn, "SELECT AVG(Waarde) AS AveragePrice FROM DataTabel WHERE ID =2;" );
+$res = mysqli_fetch_array($sql);
+$gemlicht = $res['AveragePrice'];
+$sql="SELECT * FROM DataTabel WHERE ID = 2";
+$result = mysqli_query($conn,$sql);
+while($row = mysqli_fetch_array($result)) {	
+	$lastlicht=$row['Waarde'];
+}
 
 header('Content-type: text/xml;charset=iso-8859-1');
 echo "<?xml version='1.0'  encoding='UTF-8' ?> " . PHP_EOL;
@@ -20,12 +46,27 @@ echo "<channel>" . PHP_EOL;
 
 echo "<title>RSS Feed</title>" . PHP_EOL;
 
+echo "<TemperatuurSensor>" . PHP_EOL;
+echo "<title>maximum</title>" . PHP_EOL;
+echo "<description>".$maximumtemp."</description>" . PHP_EOL;
+echo "<title>minimum</title>" . PHP_EOL;
+echo "<description>".$minimumtemp."</description>" . PHP_EOL;
+echo "<title>LastValue</title>" . PHP_EOL;
+echo "<description>".$lasttemp."</description>" . PHP_EOL;
+echo "<title>Average</title>" . PHP_EOL;
+echo "<description>".$gemtemp."</description>" . PHP_EOL;
+echo "</TemperatuurSensor>" . PHP_EOL;
+echo "<LichtSensor>" . PHP_EOL;
+echo "<title>maximum</title>" . PHP_EOL;
+echo "<description>".$maximumlicht."</description>" . PHP_EOL;
+echo "<title>minimum</title>" . PHP_EOL;
+echo "<description>".$minimumlicht."</description>" . PHP_EOL;
+echo "<title>LastValue</title>" . PHP_EOL;
+echo "<description>".$lastlicht."</description>" . PHP_EOL;
+echo "<title>Average</title>" . PHP_EOL;
+echo "<description>".$gemlicht."</description>" . PHP_EOL;
+echo "</LichtSensor>" . PHP_EOL;
 
-echo "<item>" . PHP_EOL;
-echo "<title>max</title>" . PHP_EOL;
-echo "<description>".$maximum."<\description>" . PHP_EOL;
-
-echo "</item>" . PHP_EOL;
 echo"<language>en-us</language>" . PHP_EOL;
 
 
